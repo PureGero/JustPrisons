a21f4849aa749dcefe5b800fd9495dcb82a9ff4d
PureGero <puregero@gmail.com>
2020-11-02 14:25:49 +1000

    Disable hunger

--- src/main/java/net/justminecraft/prisons/spawn/SpawnListener.java
+++ src/main/java/net/justminecraft/prisons/spawn/SpawnListener.java
@@ -1,7 +1,9 @@
 package net.justminecraft.prisons.spawn;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 public class SpawnListener implements Listener {
@@ -13,10 +15,17 @@ public class SpawnListener implements Listener {
     }
 
     @EventHandler
-    public void onSpawn(PlayerSpawnLocationEvent e) {
-        e.setSpawnLocation(spawnManager.getSpawn().getSpawnLocation().add(Math.random(), 0, Math.random()));
+    public void onSpawn(PlayerSpawnLocationEvent event) {
+        event.setSpawnLocation(spawnManager.getSpawn().getSpawnLocation().add(Math.random(), 0, Math.random()));
 
-        e.getPlayer().setFoodLevel(20);
+        event.getPlayer().setFoodLevel(20);
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        if (event.getFoodLevel() < ((Player) event.getEntity()).getFoodLevel()) {
+            event.setCancelled(true);
+        }
     }
 
 }
