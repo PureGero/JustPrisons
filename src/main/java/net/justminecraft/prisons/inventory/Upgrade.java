f61e86f26e15424fefa5941aa6650d89332077f2
PureGero <puregero@gmail.com>
2020-11-02 16:43:14 +1000

    Add starter pickaxe

---
+++ src/main/java/net/justminecraft/prisons/inventory/Upgrade.java
@@ -0,0 +1,77 @@
+package net.justminecraft.prisons.inventory;
+
+import org.bukkit.ChatColor;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Upgrade {
+    EFFICIENCY("Efficiency", ChatColor.LIGHT_PURPLE, Enchantment.DIG_SPEED),
+    FORTUNE("Fortune", ChatColor.GRAY, Enchantment.LOOT_BONUS_BLOCKS),
+    UNBREAKING("Unbreaking", ChatColor.DARK_BLUE, Enchantment.DURABILITY),
+    LUCK("Luck", ChatColor.DARK_GREEN),
+    LOOTING("Looting", ChatColor.DARK_RED),
+    CHARITY("Charity", ChatColor.GREEN),
+    LURE("Lure", ChatColor.DARK_AQUA),
+    SPEED_BOOST("Speed boost", ChatColor.GOLD);
+
+    private final String name;
+    private final ChatColor color;
+    private final Enchantment enchantment;
+
+    Upgrade(String name, ChatColor color) {
+        this.name = name;
+        this.color = color;
+        this.enchantment = null;
+    }
+
+    Upgrade(String name, ChatColor color, Enchantment enchantment) {
+        this.name = name;
+        this.color = color;
+        this.enchantment = enchantment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ChatColor getColor() {
+        return color;
+    }
+
+    public Enchantment getEnchantment() {
+        return enchantment;
+    }
+
+    public static void setUpgrade(ItemStack item, Upgrade upgrade, BigInteger level) {
+        ItemMeta meta = item.getItemMeta();
+
+        List<String> lore = meta.getLore();
+
+        if (lore == null) {
+            lore = new ArrayList<>();
+        }
+
+        lore.removeIf(line -> line.contains(upgrade.getName()));
+
+        lore.add(upgrade.getColor() + " +" + level.toString() + " " + upgrade.getName());
+
+        meta.setLore(lore);
+
+        item.setItemMeta(meta);
+
+        int enchantmentLevel = level.intValue();
+
+        if (level.compareTo(BigInteger.valueOf(32767)) > 0) {
+            enchantmentLevel = 32767;
+        }
+
+        if (upgrade.getEnchantment() != null) {
+            item.addUnsafeEnchantment(upgrade.getEnchantment(), enchantmentLevel);
+        }
+    }
+}
--- src/main/java/net/justminecraft/prisons/spawn/SpawnListener.java
+++ src/main/java/net/justminecraft/prisons/spawn/SpawnListener.java
@@ -1,11 +1,22 @@
 package net.justminecraft.prisons.spawn;
 
+import net.justminecraft.prisons.inventory.Upgrade;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SpawnListener implements Listener {
 
@@ -20,6 +31,24 @@ public class SpawnListener implements Listener {
         event.setSpawnLocation(spawnManager.getSpawn().getSpawnLocation().add(Math.random(), 0, Math.random()));
 
         event.getPlayer().setFoodLevel(20);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (!event.getPlayer().hasPlayedBefore()) {
+            Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "Welcome " + event.getPlayer().getName() + " to JustPrisons!");
+
+            ItemStack starterPickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
+
+            ItemMeta meta = starterPickaxe.getItemMeta();
+            meta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Starter Pickaxe");
+            meta.setLore(new ArrayList<>(Arrays.asList("", ChatColor.WHITE + "Right-click this pickaxe to upgrade it")));
+            starterPickaxe.setItemMeta(meta);
+
+            Upgrade.setUpgrade(starterPickaxe, Upgrade.EFFICIENCY, BigInteger.TEN);
+
+            event.getPlayer().getInventory().addItem(starterPickaxe);
+        }
     }
 
     @EventHandler
