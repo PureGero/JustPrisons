fdc1a0e7e20d68dfe3377d0d8902737312848d96
PureGero <puregero@gmail.com>
2020-11-04 19:50:58 +1000

    Add multis to keys

--- src/main/java/net/justminecraft/prisons/inventory/Multi.java
+++ src/main/java/net/justminecraft/prisons/inventory/Multi.java
@@ -1,9 +1,27 @@
 package net.justminecraft.prisons.inventory;
 
+import net.justminecraft.prisons.Translate;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Multi {
     public static void giveMulti(int multi, Player player) {
-        player.sendMessage("You got a " + multi + "x multi!");
+        ItemStack item = new ItemStack(Material.GOLDEN_APPLE, 1, (short) 1);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.LIGHT_PURPLE + "" + multi + "x Multi");
+        meta.setLore(new ArrayList<>(Collections.singletonList(ChatColor.LIGHT_PURPLE + "x" + multi + " multiplier for 15 minutes")));
+        item.setItemMeta(meta);
+        item.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, multi);
+
+        if (player.getInventory().addItem(item).size() == 0) {
+            Translate.sendMessage(player, "prisons.keys.receive", meta.getDisplayName().substring(0, 2), meta.getDisplayName());
+        }
     }
 }
