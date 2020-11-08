8c7e899b8b596cd8651dc38aa501bbbef59100b5
PureGero <puregero@gmail.com>
2020-11-08 15:22:55 +1000

    Prevent key chests from being opened

--- src/main/java/net/justminecraft/prisons/inventory/KeyListener.java
+++ src/main/java/net/justminecraft/prisons/inventory/KeyListener.java
@@ -3,6 +3,7 @@ package net.justminecraft.prisons.inventory;
 import net.justminecraft.prisons.PrisonsPlugin;
 import net.justminecraft.prisons.spawn.SpawnListener;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -10,24 +11,31 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class KeyListener implements Listener {
 
-    private static Map<String, Location> keyChests = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    private static Map<String, Location> keysToChests = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    private static Map<Location, String> chestsToKeys = new HashMap<>();
     static {
-        keyChests.put(Key.VOTE_KEY, new Location(Bukkit.getWorlds().get(0), 4, 61, 19));
-        keyChests.put(Key.COMMON_KEY, new Location(Bukkit.getWorlds().get(0), 2, 61, 20));
-        keyChests.put(Key.RARE_KEY, new Location(Bukkit.getWorlds().get(0), 0, 61, 21));
-        keyChests.put(Key.EPIC_KEY, new Location(Bukkit.getWorlds().get(0), -2, 61, 20));
-        keyChests.put(Key.LEGENDARY_KEY, new Location(Bukkit.getWorlds().get(0), -4, 61, 19));
+        addKeyChest(Key.VOTE_KEY, new Location(Bukkit.getWorlds().get(0), 4, 61, 19));
+        addKeyChest(Key.COMMON_KEY, new Location(Bukkit.getWorlds().get(0), 2, 61, 20));
+        addKeyChest(Key.RARE_KEY, new Location(Bukkit.getWorlds().get(0), 0, 61, 21));
+        addKeyChest(Key.EPIC_KEY, new Location(Bukkit.getWorlds().get(0), -2, 61, 20));
+        addKeyChest(Key.LEGENDARY_KEY, new Location(Bukkit.getWorlds().get(0), -4, 61, 19));
+    }
+
+    private static void addKeyChest(String key, Location location) {
+        keysToChests.put(key, location);
+        chestsToKeys.put(location, key);
     }
 
     public KeyListener(PrisonsPlugin plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
 
-        for (Map.Entry<String, Location> entry : keyChests.entrySet()) {
+        for (Map.Entry<String, Location> entry : keysToChests.entrySet()) {
             SpawnListener.summonArmourStand(entry.getKey(), entry.getValue().clone().add(0.5, -1, 0.5));
         }
     }
@@ -38,14 +46,20 @@ public class KeyListener implements Listener {
                 && event.getItem().getType() == Material.TRIPWIRE_HOOK
                 && event.getItem().getItemMeta() != null
                 && event.getItem().getItemMeta().getDisplayName() != null
-                && keyChests.containsKey(event.getItem().getItemMeta().getDisplayName())
-                && keyChests.get(event.getItem().getItemMeta().getDisplayName()).getBlock().equals(event.getClickedBlock())) {
+                && keysToChests.containsKey(event.getItem().getItemMeta().getDisplayName())
+                && keysToChests.get(event.getItem().getItemMeta().getDisplayName()).getBlock().equals(event.getClickedBlock())) {
+            // Right-clicked a key chest with the right key
             ItemStack item = event.getItem();
             do {
                 Key.useKey(event.getPlayer(), item);
                 item.setAmount(item.getAmount() - 1);
             } while (event.getPlayer().isSneaking() && item.getAmount() > 0);
             event.getPlayer().setItemInHand(item);
+            event.setCancelled(true);
+        } else if (event.getClickedBlock() != null
+                && chestsToKeys.containsKey(event.getClickedBlock().getLocation())) {
+            // Right-clicked a key chest without a key
+            event.getPlayer().sendMessage(ChatColor.RED + "This chest only accepts " + chestsToKeys.get(event.getClickedBlock().getLocation()) + "s");
             event.setCancelled(true);
         }
     }
